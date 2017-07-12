c99dbdebb28fe094f1f2310be7326f132baa5932
Alex Saunders <acsauk@gmail.com>
2017-07-12 21:30:51 +0100

    updates seed file with dummy seed data

--- notes-saver/db/seeds/development/notes_seed.js
+++ notes-saver/db/seeds/development/notes_seed.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('notes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('notes').insert([
+        {id: 1, note_content: 'Note content goes here'},
+        {id: 2, note_content: 'Some more note content goes here'},
+        {id: 3, note_content: 'And yet more note content goes here'}
       ]);
     });
 };
