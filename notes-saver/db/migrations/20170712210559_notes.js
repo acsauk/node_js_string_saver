8b81c5a6a150dd2b9bc335bd73106ce90e6efe2a
Alex Saunders <acsauk@gmail.com>
2017-07-12 21:20:22 +0100

    populate migration file to up() and down() notes table. Decides on note_content as column to save user notes in

--- notes-saver/db/migrations/20170712210559_notes.js
+++ notes-saver/db/migrations/20170712210559_notes.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('notes' => (table) {
+    table.increments()
+    table.string('note_content').notNullable()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('notes')
 };
