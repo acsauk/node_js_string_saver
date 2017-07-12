035ae8ac7135b94c006ab0ad3f131c938435a418
Alex Saunders <acsauk@gmail.com>
2017-07-12 21:47:09 +0100

    updates app.js error handling to return error in json and blocks error messages when in production mode

--- notes-saver/app.js
+++ notes-saver/app.js
@@ -34,15 +34,27 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
+// dev handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    // render the error page
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+});
 
+// production handler
+app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
