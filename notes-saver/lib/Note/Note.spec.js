50e02cd132ba219c70b577da9753482cdc5fee65
Alex Saunders <acsauk@gmail.com>
2017-07-13 15:17:47 +0100

    creates failing test for note#create()

--- notes-saver/lib/Note/Note.spec.js
+++ notes-saver/lib/Note/Note.spec.js
@@ -19,14 +19,24 @@ describe('Note module', () => {
     })
 
     it('should return a promise', () => {
-
       const noteCreateActualResult = Note.create()
       expect(noteCreateActualResult.then).to.be.a('function')
       expect(noteCreateActualResult.catch).to.be.a('function')
     })
 
-    it('should add an entry to Notes table', function * () {
-
+    it('should add an entry to Notes table', (done) => {
+      chai.request(server)
+        .post('/notes')
+        .send({
+          note_contents: 'I am a note!'
+        })
+        .end(function(err, res) {
+          expect(res).to.have.status(200)
+          expect(res).to.be.json
+          expect(res.body).to.be.a('object')
+          expect(res.body.note_contents).to.eql('I am a note!')
+          done()
+        })
     })
   })
 })
